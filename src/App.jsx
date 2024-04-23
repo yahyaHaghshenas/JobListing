0f51d67840b3ec92b054ec2c4ff2bde4b26f2202
yahya haqshenas <104975467+yahyaHaghshenas@users.noreply.github.com>
2024-04-24 01:16:04 +0800

    routing initialized
    routing and fetching data with API have been enhanced

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from './components/HomeCards';
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider,} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import JobPage from './pages/JobPage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage/>} />
+    <Route path='/jobs' element={<JobsPage/>} />
+    <Route path='/jobs/:id' element={<JobPage/>} />
+  </Route>
+)
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar/>
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs/>
-    </>
-  );
+  return <RouterProvider router={router} />
 };
 
 export default App
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useState } from "react"
 import { FaMapMarker } from 'react-icons/fa';
 
@@ -40,12 +41,12 @@ const JobListing = ({job}) => {
                     <FaMapMarker className="inline text-lg mb-1 mr-1" />
                     {job.location}
                   </div>
-                  <a
-                    href={`/job/${job.id}`}
+                  <Link
+                    to={`/jobs/${job.id}`}
                     className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                   > 
                   Read More
-                  </a>
+                  </Link>
                 </div>
               </div>
           </div>
