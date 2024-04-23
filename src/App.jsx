836276b02d1b92444c2624f3e940ae4470d6acfb
yahya haqshenas <104975467+yahyaHaghshenas@users.noreply.github.com>
2024-04-23 16:21:05 +0800

    landing page initialized
    landing page and its components has been initialized in this commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from './components/HomeCards';
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs/>
+    </>
+  );
+};
+
+export default App
