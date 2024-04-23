aaa17085766dfc385f67c0beea90e1c5d941f89b
yahya haqshenas <104975467+yahyaHaghshenas@users.noreply.github.com>
2024-04-24 03:59:14 +0800

    CRUD enhanced
    add, delete and update functionalities implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,58 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider,} 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
-import JobPage from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage/>} />
-    <Route path='/jobs' element={<JobsPage/>} />
-    <Route path='/jobs/:id' element={<JobPage/>} />
-  </Route>
-)
-);
 
 const App = () => {
+  //add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  //update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify(job),
+    });
+    return;
+  };
+
+  //delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage/>} />
+      <Route path='/jobs' element={<JobsPage/>} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+      <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob} />} loader={jobLoader} />
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
+
+    </Route>
+  )
+  );
+
+
   return <RouterProvider router={router} />
 };
 
